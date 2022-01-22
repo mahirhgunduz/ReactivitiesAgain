ff833eec1f8fda2a29f7f94fb1aab65efe8e3c25
mahir <mahir@mahirs-Mac-mini.local>
2022-01-22 21:59:25 +0300

    Initial

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,26 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+    
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+
+        public DbSet<Activity> Activities { get; set; }
+    } 
+}
